884c807c744aaac56aabd702ba58f5dcf621a3b3
Luis Dasta <ldasta786@gmail.com>
2019-03-24 21:23:31 -0500

    add empty reports controller

---
+++ camera-trigger-api-core/Controllers/ReportsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using camera_trigger_api_core.Contexts;
+using camera_trigger_api_core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace camera_trigger_api_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private TriggerContext _ctx;
+        public ReportsController(TriggerContext ctx)
+        {
+            _ctx = ctx;
+        }
+        // GET api/triggers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Trigger>>> GetAsync()
+        {
+            Request.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return await _ctx.Triggers.ToListAsync();
+        }
+    }
+}
